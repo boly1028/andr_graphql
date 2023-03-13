810a2668a6abf7ae5d387f87de4581bed6cdf00b
Arun Bokkala <arunkumar.bokkala@gmail.com>
2023-03-13 14:27:58 +0530

    add aws cdk scripts files

---
+++ cdk/singleAccount/bin/app.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { AndrHttpApiStack } from '../lib/andr-httpApi-stack';
+import { AndrVpclinkEcsStack } from '../lib/andr-vpc-link-ecs-stack';
+import { AndrVpcStack } from '../lib/andr-vpc-stack';
+
+const envUSA = { region: 'us-east-1' };
+
+const app = new cdk.App();
+const env_name = app.node.tryGetContext('env_name')
+const ENV = String(env_name).toUpperCase();
+
+const rtbVpcStack = new AndrVpcStack(app, "AndrVpcStack", { env: envUSA });
+const rtbVpclinkEcsStack = new AndrVpclinkEcsStack(app, `AndrFargateVpclinkStack${ENV}`, rtbVpcStack.producerVPC, { env: envUSA });
+new AndrHttpApiStack(app, `AndrHttpApiStack${ENV}`, rtbVpcStack.consumerVPC, rtbVpclinkEcsStack.httpVpcLink, rtbVpclinkEcsStack.httpApiListener , { env: envUSA });
